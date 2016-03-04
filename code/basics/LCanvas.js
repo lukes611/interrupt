be571fb2db08554f8cd73525e1007f34b6e2ea06
lukes611 <lukes611@gmail.com>
2016-03-04 23:54:05 +1000

    testing

--- code/basics/LCanvas.js
+++ code/basics/LCanvas.js
@@ -15,14 +15,31 @@ LCanvas.prototype.setFill = function(color){
 };
 
 LCanvas.prototype.rect = function(x, y, w, h){
+	this.context.beginPath();
 	this.context.rect(x, y, w, h);
 	this.context.stroke();
 };
 
 
 LCanvas.prototype.rectf = function(x, y, w, h){
+	this.context.beginPath();
 	this.context.rect(x, y, w, h);
 	this.context.fill();
 };
+
+LCanvas.prototype.transform = function(m){
+	this.context.transform(m.arr[0], m.arr[3], m.arr[1], m.arr[4], m.arr[2], m.arr[5]);
+};
+
+LCanvas.prototype.resetTransform = function(){
+	this.context.setTransform(1, 0, 0, 1, 0, 0);
+};
+
+LCanvas.prototype.line = function(x, y, x2, y2){
+	this.context.beginPath();
+	this.context.moveTo(x, y);
+	this.context.lineTo(x2, y2);
+	this.context.stroke();
+}; 
 
 
