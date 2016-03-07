2a3ce47e8b060c15eb8c410f9a6b8bb58fdf4e3a
lukes611 <lukes611@gmail.com>
2016-03-08 09:02:16 +1000

    adding map component

--- code/basics/Liso.js
+++ code/basics/Liso.js
@@ -29,8 +29,23 @@ Liso.screen2Tile = function(p){
 	var p2 = new LV2(0, -1);
 	p1 = m.multLV2(p1);
 	p2 = m.multLV2(p2);
-	return new LV2(p1.dot(p) * 2, p2.dot(p) * 2);
+	return new LV3(p1.dot(p) * 2, 0, p2.dot(p) * 2);
 };
+
+Liso.directionRight = function(){
+	var angle = 45 * 0.0174533;
+	var cosine = Math.cos(angle);
+	var sinus = Math.sin(angle);
+	return new LV3(cosine, 0, sinus);
+};
+
+Liso.directionDown = function(){
+	var angle = -45 * 0.0174533;
+	var cosine = Math.cos(angle);
+	var sinus = Math.sin(angle);
+	return new LV3(cosine, 0, sinus);
+};
+
 
 LMat3.iso = function(){
 	return LMat3.rotateX(30).mult(LMat3.rotateY(45).mult(LMat3.rotateX(90)));
@@ -43,8 +58,42 @@ LMat3.iiso = function(){
 function LisoMap(w, h){
 	this.w = w;
 	this.h = h;
-	this.focus = new LV2(0, 0);
+	this.focus = new LV3(0, 0, 0);
+	this.tileSize = 25;
+	this.tiles = new Array(this.w * this.h);
+	for(var i = 0; i < this.tiles.length; i++) this.tiles[i] = 'orange';
 }
+
+LisoMap.prototype.get = function(x, y){return this.tiles[this.w * y + x]; };
+LisoMap.prototype.set = function(x, y, v){this.tiles[this.w * y + x] = v; };
+LisoMap.prototype.inBounds = function(x, y){return x >= 0 && y >= 0 && x < this.w && y < this.h;};
+
+LisoMap.prototype.moveRight = function(scalar){
+	this.focus.iadd(Liso.directionRight().scale(scalar));
+};
+LisoMap.prototype.moveLeft = function(scalar){
+	this.focus.isub(Liso.directionRight().scale(scalar));
+};
+LisoMap.prototype.moveUp = function(scalar){
+	this.focus.isub(Liso.directionDown().scale(scalar));
+};
+LisoMap.prototype.moveDown = function(scalar){
+	this.focus.iadd(Liso.directionDown().scale(scalar));
+};
+LisoMap.prototype.screen2Position = function(screenPoint){
+	var rv = Liso.screen2Tile(screenPoint);
+	rv.iadd(this.focus);
+	rv.idiv(this.tileSize);
+	return rv;
+};
+LisoMap.prototype.screen2Tile = function(screenPoint){
+	var rv = Liso.screen2Tile(screenPoint);
+	rv.iadd(this.focus);
+	rv.idiv(this.tileSize);
+	rv.floor();
+	return rv;	
+};
+
 
 
 
