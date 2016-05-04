91eff3bbc5a09273dd24661b06d0f737124c3f3c
lukes611 <lukes611@gmail.com>
2016-05-04 10:57:53 +1000

    updating LCanvas with comments

--- code/basics/LCanvas.js
+++ code/basics/LCanvas.js
@@ -1,5 +1,25 @@
+/*
+	a canvas object:
+	variables:
+		canvas - the canvas html5 object
+		w, h   - the width and height of the canvas
+		context - the context used for drawing
+
+	functions:
+		getCenter -> returns the center of the screen as LV2
+		setStroke, setFill -> sets the stroke and fill colors
+		rect, rectf -> draws/fills a rectangle
+		circle, circlef -> draws/fills a circle
+		transform -> set the transform to an LMat3 matrix
+		resetTransform -> resets the transform
+		line -> draws a line
+		dimage -> draws an LCanvas as an image to the screen
+
+	static functions:
+		image -> loads an image as a canvas object
+*/
+
 function LCanvas(w, h){
-	this.idName = "myCanvas";
 	this.canvas = document.createElement('canvas');
 	this.canvas.width = w;
 	this.canvas.height = h;
@@ -30,6 +50,18 @@ LCanvas.prototype.rect = function(x, y, w, h){
 LCanvas.prototype.rectf = function(x, y, w, h){
 	this.context.beginPath();
 	this.context.rect(x, y, w, h);
+	this.context.fill();
+};
+
+LCanvas.prototype.circle = function(x, y, r){
+	this.context.beginPath();
+	this.context.arc(x, y, r, 0, 2 * Math.PI, false);
+	this.context.stroke();
+};
+
+LCanvas.prototype.circlef = function(x, y, r){
+	this.context.beginPath();
+	this.context.arc(x, y, r, 0, 2 * Math.PI, false);
 	this.context.fill();
 };
 
